364eeeba4a6eaadc80680c576bb890dfc98b6661
belgort-clark <belgort@clark.edu>
2019-04-21 17:37:45 -0700

    Added rubric and stub js code

--- js/lab5.js
+++ js/lab5.js
@@ -1,1 +1,28 @@
 // lab5.js
+
+// declare questions array and populate it with 10 questions/answers
+
+// variable declarations
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+// function to handle asking questions and getting answers
+// the question parameter is an array
+function askQuestion(question){
+    // your code goes here
+}
+
+function quizSummary(){
+    // your code goes here
+}
+
+function takeQuiz(){
+
+	let userName = prompt("Before we start the quiz please entry your name");
+
+	// loop thru questions array
+    // and call the askQuestion function during each iteration
+
+	// call the QuizSummary function to display the results
+	quizSummary();
+} // end of takeQuiz funciton
